6a7ec982aba7dd26688b481076afc2346284b5eb
BBAYARD <admin.bbayard@SX04NELLT0200.ad.ponet>
2024-11-27 14:14:27 +0100

    removing product is now deleting check(s) => stock(s) => product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import { map, Observable, forkJoin } from 'rxjs';
+import { map, Observable, forkJoin, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -58,18 +58,42 @@ export class ProductService {
 
   }
 
-  removeProduct(id: number): void {
+
+  removeCheck(id: number): void {
+    this.getCheckIdByProductId(id).subscribe(checks => {
+      if (checks.length === 0) {
+        // Si aucun check n'est associé, supprimer directement le stock
+        this.removeStock(id);
+      } else {
+        console.log('ID des stocks associés au produit:', checks.map(check => check.id));
+        // Sinon, supprimer les checks associés avant de supprimer le stock
+        const deleteChecksRequests = checks.map(check => this.httpClient.delete(`api/checks/${check.id}`));
+        
+        forkJoin(deleteChecksRequests).subscribe({
+          next: () => {
+            this.removeStock(id);
+          },
+          error: (err) => {
+            console.error('Erreur lors de la suppression des Checks :', err);
+          }
+        });
+      }
+    });
+  }
+
+  removeStock(id: number): void {
     this.getStockIdByProductId(id).subscribe(stocks => {
       if (stocks.length === 0) {
         // Si aucun stock n'est associé, supprimer directement le produit
-        this.deleteProduct(id);
+        this.removeProduct(id);
       } else {
+        console.log('ID des stocks associés au produit:', stocks.map(stock => stock.id));
         // Sinon, supprimer les stocks associés avant de supprimer le produit
         const deleteRequests = stocks.map(stock => this.httpClient.delete(`api/stocks/${stock.id}`));
         
         forkJoin(deleteRequests).subscribe({
           next: () => {
-            this.deleteProduct(id);
+            this.removeProduct(id);
           },
           error: (err) => {
             console.error('Erreur lors de la suppression des stocks :', err);
@@ -79,7 +103,7 @@ export class ProductService {
     });
   }
   
-  deleteProduct(id: number): void {
+  removeProduct(id: number): void {
     this.productArray = this.productArray.filter(product => product.id !== id);
     this.httpClient.delete(`api/products/${id}`).subscribe(() => {
       this.refreshProducts();
@@ -95,6 +119,18 @@ export class ProductService {
       map((stocks: any[]) => stocks.filter(stock => stock.product.id === productId))
     );
   }
+
+  getCheckIdByProductId(productId: number): Observable<any[]> {
+    return this.getStockIdByProductId(productId).pipe(  // Appel de la méthode pour obtenir les stocks associés au produit
+      switchMap(stocks => {
+        const stockIds = stocks.map(stock => stock.id);  // Récupération des IDs des stocks associés au produit
+        return this.httpClient.get<any[]>('api/checks').pipe(
+          map((checks: any[]) => checks.filter(check => stockIds.includes(check.stock.id)))  // Filtre les checks où stock.id est dans stockIds
+        );
+      })
+    );
+  }
+  
 
   /*getStockByProductId(id: number) {
     return this.httpClient.get<number[]>('/api/stocks/{id}')
